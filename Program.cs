379482acb7b09021a21359869eec736333a8f84b
Vitaly Kovalev <kovalevvitalii@mail.ru>
2023-03-21 22:07:18 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Anonimus
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var i = 5;
+
+            var product = new
+            {
+                Name = "Milk",
+                Energy = 10,
+                
+            };
+        }
+    }
+}
